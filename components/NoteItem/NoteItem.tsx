30388619013a1337aba4b0e5573e2d97527c6dfb
Pashka Antonov <pavelcreed222@gmail.com>
2025-10-02 20:52:06 +0300

    hw-06

--- components/NoteItem/NoteItem.tsx
+++ components/NoteItem/NoteItem.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 interface NoteItemProps {
   note: Note;
-  onDelete: (id: number) => void;
+  onDelete: (id: string) => void;
 }
 
 export default function NoteItem({ note, onDelete }: NoteItemProps) {
